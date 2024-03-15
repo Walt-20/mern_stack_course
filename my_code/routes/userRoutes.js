141e91e53986e8268a3c7eefb7bb9d5c862117af
Walt-20 <walt.c0des20@gmail.com>
2024-03-15 10:19:29 -0600

    created CRUD infrustructure

---
+++ my_code/routes/userRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const usersController = require('../controllers/usersController')
+
+router.route('/')
+    .get()
+    .post()
+    .patch()
+    .delete()
+
+module.exports = router
